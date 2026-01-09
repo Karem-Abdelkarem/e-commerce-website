547b662056cf57e3e0030e74b02070d6d1ddb640
Kareem Abdelkarem <km6263458@gmail.com>
2026-01-10 00:47:56 +0200

    feat: add responsive style

--- src/components/layout/Banner.jsx
+++ src/components/layout/Banner.jsx
@@ -9,12 +9,12 @@ const Banner = () => {
 
   return (
     <section className="max-w-292.5 mx-auto mb-17.5 px-3 bg-primary">
-      <div className="flex items-center justify-between py-17.5 pl-14 pr-11">
+      <div className="flex items-center justify-between py-17.5 pl-0 md:pl-14 pr-11">
         <div className="flex flex-col gap-8">
           <h4 className="font-poppins text-[16px] font-semibold text-destructive">
             {t("Categories")}
           </h4>
-          <h2 className="font-inter text-5xl font-semibold text-white leading-15">
+          <h2 className="font-inter text-4xl md:text-5xl font-semibold text-white leading-15">
             {t("Enhance Your Music Experience")}
           </h2>
           <BannerCountdown endDate="2027-01-30T23:59:59" />
@@ -22,7 +22,7 @@ const Banner = () => {
             <Button variant="destructive">{t("Buy Now")}</Button>
           </Link>
         </div>
-        <div className="">
+        <div className="hidden sm:block">
           <img
             src={JblImage}
             alt=""

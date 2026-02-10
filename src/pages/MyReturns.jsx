652cd6eea148d6162ff3424a36f135d752d09464
Kareem Abdelkarem <km6263458@gmail.com>
2026-02-10 20:38:30 +0200

    feat: add MyReturns component

---
+++ src/pages/MyReturns.jsx
@@ -0,0 +1,9 @@
+const MyReturns = () => {
+  return (
+    <section className="w-210 bg-white py-10 px-20 shadow-lg rounded-md font-poppins">
+      <h1 className="text-secondary text-2xl font-medium mb-4">My Returns</h1>
+      <p className="text-gray-500 py-16 text-center">No returns yet</p>
+    </section>
+  );
+};
+export default MyReturns;

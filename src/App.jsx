93c5aac4d2fb2a101585c69a24371d46933b3956
Kareem Abdelkarem <km6263458@gmail.com>
2026-02-10 16:33:06 +0200

    feat: add pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,21 @@ import Contact from "./pages/Contact";
 import About from "./pages/About";
 import SignUp from "./pages/SignUp";
 import Shop from "./pages/Shop";
+import Login from "./pages/Login";
+import ForgetPassword from "./pages/ForgetPassword";
+import Wishlist from "./pages/Wishlist";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import MyAcount from "./pages/MyAcount";
+import Bill from "./pages/Bill";
+import MyOrders from "./pages/MyOrders";
+import ProductDetails from "./pages/ProductDetails";
+import ErrorPage from "./pages/ErrorPage";
+import Profile from "./pages/Profile";
+import AddressBook from "./pages/AddressBook";
+import PaymentOptions from "./pages/PaymentOptions";
+import MyReturns from "./pages/MyReturns";
+import MyCancellations from "./pages/MyCancellations";
 
 let router = createBrowserRouter([
   {
@@ -12,8 +27,12 @@ let router = createBrowserRouter([
     Component: Layout,
     children: [
       {
-        path: "/",
+        index: true,
         Component: Home,
+      },
+      {
+        path: "/shop",
+        Component: Shop,
       },
       {
         path: "/contact",
@@ -28,8 +47,66 @@ let router = createBrowserRouter([
         Component: SignUp,
       },
       {
-        path: "/shop",
-        Component: Shop,
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/forget-password",
+        Component: ForgetPassword,
+      },
+      {
+        path: "/wishlist",
+        Component: Wishlist,
+      },
+      {
+        path: "/cart",
+        Component: Cart,
+      },
+      {
+        path: "/checkout",
+        Component: Checkout,
+      },
+      {
+        path: "/bill",
+        Component: Bill,
+      },
+      {
+        path: "/account",
+        Component: MyAcount,
+        children: [
+          {
+            index: true,
+            Component: Profile,
+          },
+          {
+            path: "/account/address",
+            Component: AddressBook,
+          },
+          {
+            path: "/account/payment-options",
+            Component: PaymentOptions,
+          },
+          {
+            path: "/account/my-orders",
+            Component: MyOrders,
+          },
+          {
+            path: "/account/returns",
+            Component: MyReturns,
+          },
+          {
+            path: "/account/cancellations",
+            Component: MyCancellations,
+          },
+        ],
+      },
+      {
+        path: "/products/:id",
+        Component: ProductDetails,
+      },
+      {
+        path: "*",
+        Component: ErrorPage,
       },
     ],
   },

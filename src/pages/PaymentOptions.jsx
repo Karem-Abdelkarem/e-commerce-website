c21c4c4836eced1ed3b58100d6eb033dca1e999d
Kareem Abdelkarem <km6263458@gmail.com>
2026-02-10 19:08:25 +0200

    feat: add PaymentOptions component

---
+++ src/pages/PaymentOptions.jsx
@@ -0,0 +1,13 @@
+const PaymentOptions = () => {
+  return (
+    <section className="w-210 bg-white py-10 px-20 shadow-lg rounded-md font-poppins">
+      <h1 className="text-secondary text-2xl font-medium mb-4">
+        My Payment Options
+      </h1>
+      <p className="text-gray-500 py-16 text-center">
+        No payment methods added yet
+      </p>
+    </section>
+  );
+};
+export default PaymentOptions;

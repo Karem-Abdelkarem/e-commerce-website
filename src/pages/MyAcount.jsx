d7cc76e6f1aa25b204eec3b5eb56c0cea4e939e8
Kareem Abdelkarem <km6263458@gmail.com>
2026-02-10 20:39:30 +0200

    feat: add MyAccount page

---
+++ src/pages/MyAcount.jsx
@@ -0,0 +1,35 @@
+import { useTranslation } from "react-i18next";
+import { Link, Outlet } from "react-router-dom";
+import MyAccountSidebar from "../components/ui/my-account/MyAccountSidebar";
+import { useAuth } from "../context/AuthContext";
+
+const MyAcount = () => {
+  const { t } = useTranslation();
+  const { user } = useAuth();
+
+  return (
+    <main className="max-w-292.5 mx-auto pt-20 pb-35 px-3 font-poppins">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between gap-3 w-fit text-sm">
+          <Link to="/" className="text-muted-foreground">
+            {t("home")}
+          </Link>
+          <span className="text-muted-foreground">/</span>
+          <span className="text-primary">{t("account")}</span>
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="text-sm text-primary">Welcome!</span>
+          <span className="text-sm text-secondary">
+            {user?.firstName || ""}
+          </span>
+        </div>
+      </div>
+
+      <div className="flex items-start gap-25 mt-20">
+        <MyAccountSidebar />
+        <Outlet />
+      </div>
+    </main>
+  );
+};
+export default MyAcount;

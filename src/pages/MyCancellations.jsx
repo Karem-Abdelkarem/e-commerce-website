24fe179bd803506287f886f5990010b9919971aa
Kareem Abdelkarem <km6263458@gmail.com>
2026-02-10 20:39:01 +0200

    feat: add MyCancellations component

---
+++ src/pages/MyCancellations.jsx
@@ -0,0 +1,11 @@
+const MyCancellations = () => {
+  return (
+    <section className="w-210 bg-white py-10 px-20 shadow-lg rounded-md font-poppins">
+      <h1 className="text-secondary text-2xl font-medium mb-4">
+        My Cancellations
+      </h1>
+      <p className="text-gray-500 py-16 text-center">No cancellations yet</p>
+    </section>
+  );
+};
+export default MyCancellations;
